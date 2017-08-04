a98f5afc942bd1146aa2ef98f281d57a7a90b8a7
Beniamin Hakobyan <benohakobyan@gmail.com>
2017-08-04 23:41:46 +0400

    cjasnckn

--- MyProject/App_Start/BundleConfig.cs
+++ MyProject/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace MyProject
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
         }
     }
